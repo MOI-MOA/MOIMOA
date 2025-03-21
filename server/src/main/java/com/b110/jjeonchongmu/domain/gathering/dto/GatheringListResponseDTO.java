fbb81cc76ed73946a984ad3d79edae722b87331a
jongsoo <24ssafy@gmail.com>
2025-03-21 11:04:27 +0900

    0321tmp

--- server/src/main/java/com/b110/jjeonchongmu/domain/gathering/dto/GatheringListResponseDTO.java
+++ server/src/main/java/com/b110/jjeonchongmu/domain/gathering/dto/GatheringListResponseDTO.java
@@ -1,4 +1,21 @@
 package com.b110.jjeonchongmu.domain.gathering.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import java.util.List;
+
+@Builder
+@Data
 public class GatheringListResponseDTO {
+    private Long gatheringId;
+    private String gatheringName;
+    private String gatheringIntroduction;
+    private String depositDate;
+    private Long basicFee;
+    private int penaltyRate;
+    private Integer memberCount;
+    private Long gatheringDeposit;
+
+    private List<GatheringDTO> gatherings;
+
 }
