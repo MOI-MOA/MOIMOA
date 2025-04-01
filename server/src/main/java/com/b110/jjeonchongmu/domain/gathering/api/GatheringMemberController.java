4e14797c9700d0c4f3fce02c2b80a7775aa9e149
baehanjin <bae9954@naver.com>
2025-04-01 17:46:17 +0900

    fix: gathering

--- server/src/main/java/com/b110/jjeonchongmu/domain/gathering/api/GatheringMemberController.java
+++ server/src/main/java/com/b110/jjeonchongmu/domain/gathering/api/GatheringMemberController.java
@@ -1,9 +1,9 @@
 package com.b110.jjeonchongmu.domain.gathering.api;
 
-import com.b110.jjeonchongmu.domain.gathering.dto.InviteResponseDTO;
-import com.b110.jjeonchongmu.domain.gathering.dto.MemberListResponseDTO;
-import com.b110.jjeonchongmu.domain.gathering.dto.MemberManageResponseDTO;
+import com.b110.jjeonchongmu.domain.gathering.dto.*;
+import com.b110.jjeonchongmu.domain.gathering.repo.GatheringRepo;
 import com.b110.jjeonchongmu.domain.gathering.service.GatheringMemberService;
+import com.b110.jjeonchongmu.global.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class GatheringMemberController {
     private final GatheringMemberService gatheringMemberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     /**
      * 모임 초대링크 생성 (총무)
@@ -51,13 +52,57 @@ public class GatheringMemberController {
     /**
      * 모임 참여 수락 (총무)
      */
-    @PostMapping("/{gatheringId}/accept/{userId}")
+    @PostMapping("/{gatheringId}/members/{memberId}/accept")
     public ResponseEntity<String> acceptGathering(
-            @PathVariable Long gatheringId,
-            @PathVariable Long userId) {
-        gatheringMemberService.acceptGathering(gatheringId, userId);
+            @PathVariable Long gatheringId, @PathVariable Long memberId) {
+//        Long userId = jwtTokenProvider.getUserId();
+        Long userId = 1L;
+        gatheringMemberService.acceptGathering(gatheringId, memberId, userId);
         return ResponseEntity.status(201).body("모임 참여 수락");
     }
+
+    /**
+     * 모임 참여 거부 (총무)
+     */
+    @PostMapping("/{gatheringId}/members/{memberId}/reject")
+    public ResponseEntity<String> rejectGathering(
+            @PathVariable Long gatheringId, @PathVariable Long memberId) {
+//        Long userId = jwtTokenProvider.getUserId();
+        Long userId = 1L;
+        gatheringMemberService.rejectGathering(gatheringId, memberId, userId);
+        return ResponseEntity.status(201).body("모임 참여 수락");
+    }
+
+    /**
+     * 배한진 새로 추가한 api임
+     * 초대코드로 들어가는 페이지에 들어가면 호출되는 api로
+     * gathering 정보를 리턴함.
+     * 모임 참여 페이지 api
+     */
+    @GetMapping("/{gatheringId}/accept-page")
+    public ResponseEntity<GatheringAcceptPageResponseDTO> groupJoinPage(
+            @PathVariable Long gatheringId) {
+//        Long userId = jwtTokenProvider.getUserId();
+        Long userId = 1L;
+        GatheringAcceptPageResponseDTO response = gatheringMemberService.gatheringAccpetPage(gatheringId);
+        System.out.println(response.getMemberCount());
+        return ResponseEntity.status(201).body(response);
+
+    }
+    /**
+     * 배한진 새로 추가한 api임
+     * 초대 페이지에서 참가하기 누르면 초대요청이 날라가는 api
+     */
+    @PostMapping("/{gatheringId}/join")
+    public ResponseEntity<GatheringJoinResponseDTO> groupJoinButton(
+            @PathVariable Long gatheringId) {
+//        Long userId = jwtTokenProvider.getUserId();
+        Long userId = 1L;
+        GatheringJoinResponseDTO response = gatheringMemberService.requestGatheringJoin(userId, gatheringId);
+        return ResponseEntity.status(201).body(response);
+
+    }
+
 
     /**
      * 모임 회원 관리 조회 (총무)
@@ -73,7 +118,9 @@ public class GatheringMemberController {
      */
     @GetMapping("/{gatheringId}/members")
     public ResponseEntity<MemberListResponseDTO> getMembers(@PathVariable Long gatheringId) {
-        MemberListResponseDTO response = gatheringMemberService.getMembers(gatheringId);
+//        Long userId = jwtTokenProvider.getUserId();
+        Long userId = 1L;
+        MemberListResponseDTO response = gatheringMemberService.getMembers(gatheringId, userId);
         return ResponseEntity.status(200).body(response);
     }
 
@@ -82,7 +129,9 @@ public class GatheringMemberController {
      */
     @DeleteMapping("/{gatheringId}/leave")
     public ResponseEntity<String> leaveGathering(@PathVariable Long gatheringId) {
-        gatheringMemberService.leaveGathering(gatheringId);
+//        Long userId = jwtTokenProvider.getUserId();
+        Long userId = 1L;
+        gatheringMemberService.leaveGathering(gatheringId, userId);
         return ResponseEntity.status(204).body("모임탈퇴성공");
     }
 }
---
+++ server/src/main/java/com/b110/jjeonchongmu/domain/gathering/dto/GatheringAcceptPageResponseDTO.java
@@ -0,0 +1,16 @@
+package com.b110.jjeonchongmu.domain.gathering.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GatheringAcceptPageResponseDTO {
+    private String name;
+    private String introduction;
+    private Integer memberCount;
+}
