e4d785f5531d22beaa577ec59c4cc4f92421b726
dongs <don0421865@gmail.com>
2025-04-01 12:45:09 +0900

    fix: autoPaymentResponse 로직 수정

--- server/src/main/java/com/b110/jjeonchongmu/domain/mypage/dto/auto/AutoPaymentResponse.java
+++ server/src/main/java/com/b110/jjeonchongmu/domain/mypage/dto/auto/AutoPaymentResponse.java
@@ -1,15 +1,13 @@
 package com.b110.jjeonchongmu.domain.mypage.dto.auto;
 
-import com.b110.jjeonchongmu.domain.account.repo.PersonalAccountRepo;
 import com.b110.jjeonchongmu.domain.gathering.entity.GatheringMember;
 import com.b110.jjeonchongmu.domain.user.entity.User;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -19,10 +17,9 @@ public class AutoPaymentResponse {
     private Long userId;
     private Long accountBalance;
     private List<AutoPaymentDto> autoTransfers = new ArrayList<>();
-    private PersonalAccountRepo personalAccountRepo;
-    public AutoPaymentResponse(User user) {
+    public AutoPaymentResponse(User user, Long accountBalance) {
         this.userId = user.getUserId();
-        this.accountBalance = personalAccountRepo.findByUserId(userId).get().getAccountBalance();
+        this.accountBalance = accountBalance;
         for (GatheringMember gatheringMember : user.getGatheringMembers()) {
             this.autoTransfers.add(new AutoPaymentDto(user.getUserId(), gatheringMember));
         }
