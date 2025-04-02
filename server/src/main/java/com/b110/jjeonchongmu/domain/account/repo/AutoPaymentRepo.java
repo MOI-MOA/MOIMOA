a95e380e8800600ba41ebd6a88a5ef5ee8871a8f
dongs <don0421865@gmail.com>
2025-04-02 14:19:07 +0900

    feat: 자동이체 queryDSL 추가

--- server/src/main/java/com/b110/jjeonchongmu/domain/account/repo/AutoPaymentRepo.java
+++ server/src/main/java/com/b110/jjeonchongmu/domain/account/repo/AutoPaymentRepo.java
@@ -1,10 +1,14 @@
 package com.b110.jjeonchongmu.domain.account.repo;
 
 import com.b110.jjeonchongmu.domain.account.entity.AutoPayment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AutoPaymentRepo extends JpaRepository<AutoPayment, Long> {
 
+	@Query("select ap from AutoPayment ap where ap.personalAccount.user.userId = :userId")
+	List<AutoPayment> findByUserId(Long userId);
 }
