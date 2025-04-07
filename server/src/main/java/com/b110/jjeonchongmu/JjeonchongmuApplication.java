882d3c908522ff7e1f3f287ac0c2cc829cc1b2f6
이다영 <daosp1023@gmail.com>
2025-04-07 12:50:21 +0900

    test: entityscan 추가설정

--- server/src/main/java/com/b110/jjeonchongmu/JjeonchongmuApplication.java
+++ server/src/main/java/com/b110/jjeonchongmu/JjeonchongmuApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EntityScan("com.b110.jjeonchongmu.domain")
 public class JjeonchongmuApplication {
 
 	public static void main(String[] args) {
